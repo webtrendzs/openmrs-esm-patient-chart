7fb5d8669408ff0259c7b61323366de1bab16099
Donald Kibet <chelashawdonald@yahoo.com>
2022-01-25 14:31:41 +0300

    (bug) fix error on loading deceased patient tag

--- packages/esm-patient-banner-app/src/banner-tags/deceased-tag.component.tsx
+++ packages/esm-patient-banner-app/src/banner-tags/deceased-tag.component.tsx
@@ -2,26 +2,26 @@ import React from 'react';
 import styles from './active-visit-tag.scss';
 import { useTranslation } from 'react-i18next';
 import { Tag, TooltipDefinition } from 'carbon-components-react';
+import { formatDatetime } from '@openmrs/esm-framework';
 
 interface DecasedBannerTagProps {
   patient: fhir.Patient;
 }
 const DeceasedBannerTag: React.FC<DecasedBannerTagProps> = ({ patient }) => {
   const { t } = useTranslation();
-  return (
-    patient.deceasedBoolean && (
-      <TooltipDefinition
-        align="end"
-        tooltipText={
-          <div className={styles.tooltipPadding}>
-            <h6 style={{ marginBottom: '0.5rem' }}>{t('deceased', 'Deceased')}</h6>
-          </div>
-        }
-      >
-        <Tag type="red">{t('deceased', 'Deceased')}</Tag>
-      </TooltipDefinition>
-    )
-  );
+  return patient.deceasedDateTime ? (
+    <TooltipDefinition
+      align="end"
+      tooltipText={
+        <div className={styles.tooltipPadding}>
+          <h6 style={{ marginBottom: '0.5rem' }}>{t('deceased', 'Deceased')}</h6>
+          <span>{formatDatetime(new Date(patient.deceasedDateTime))}</span>
+        </div>
+      }
+    >
+      <Tag type="red">{t('deceased', 'Deceased')}</Tag>
+    </TooltipDefinition>
+  ) : null;
 };
 
 export default DeceasedBannerTag;
--- packages/esm-patient-banner-app/src/banner-tags/deceased-tag.test.tsx
+++ packages/esm-patient-banner-app/src/banner-tags/deceased-tag.test.tsx
@@ -3,17 +3,33 @@ import { render, screen } from '@testing-library/react';
 import DeceasedBannerTag from './deceased-tag.component';
 import { mockDeceasedPatient } from '../../../../__mocks__/mockDeceasedPatient';
 import { mockPatient } from '../../../../__mocks__/patient.mock';
+import { formatDatetime } from '@openmrs/esm-framework';
+import dayjs from 'dayjs';
+
+const mockFormatDateTime = formatDatetime as jest.Mock;
+
+jest.mock('@openmrs/esm-framework', () => {
+  const originalModule = jest.requireActual('@openmrs/esm-framework');
+
+  return {
+    ...originalModule,
+    formatDatetime: jest.fn(),
+  };
+});
 
 describe('deceasedTag', () => {
+  beforeEach(() => {
+    mockFormatDateTime.mockImplementation((dateTime) => dayjs(dateTime).format('DD-MMM-YYYY HH:mm'));
+  });
   it('renders a deceased tag in the patient banner for patients who died', () => {
     render(<DeceasedBannerTag patient={mockDeceasedPatient} />);
-
+    expect(screen.getByRole('tooltip', { name: / 04-Apr-1972 00:00/i }));
     expect(screen.getByRole('button', { name: /Deceased/ })).toBeInTheDocument();
   });
 
   it('doesnot render Deceased tag for patients who are still alive', () => {
     render(<DeceasedBannerTag patient={mockPatient} />);
-
+    expect(screen.queryByRole('tooltip', { name: / 04-Apr-1972 00:00/i })).not.toBeInTheDocument();
     expect(screen.queryByRole('button', { name: /Deceased/ })).not.toBeInTheDocument();
   });
 });
